660eb189c11bac5aa8aff075bee30e86643ce9c2
tony007x <aekanan2004@gmail.com>
2024-02-01 20:00:54 +0700

    peak

---
+++ Gun.java
@@ -0,0 +1,5 @@
+public class Gun {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Peak.java
@@ -0,0 +1,5 @@
+public class Peak {
+    public static void main(String[] args) {
+        
+    }
+}
