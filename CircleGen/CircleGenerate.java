6a9903f5f3e379bb77f4b107b85a27b5e655b848
Pannawat <>
2024-02-02 17:47:37 +0700

    fix CircleGen

--- CircleGen/CircleGenerate.java
+++ CircleGen/CircleGenerate.java
@@ -2,16 +2,21 @@ package CircleGen;
 
 import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import color_lab.circle;
+
 public class CircleGenerate extends JFrame{
-    ArrayList<Circle> circleList = new ArrayList<>();
+    private ArrayList<JButton> circleList = new ArrayList<>();
 
 
-    public CircleGenerate(int n) {
+    public CircleGenerate(float hue, float saturation, float value, int n) {
         for(int i = 0; i < n; i++) {
-            Circle c = new Circle();
-            this.add(c);
+            JButton b = new JButton();
+            b.setBackground(Circle);
+            circleList.add(b);
+            this.add(b);
         }
         this.setVisible(true);
     }
