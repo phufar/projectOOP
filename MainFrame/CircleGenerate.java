16577b3e531c195e5f001d209040c51b95a65959
Pannawat <>
2024-02-02 17:16:21 +0700

    create circleGen

---
+++ MainFrame/CircleGenerate.java
@@ -0,0 +1,23 @@
+package MainFrame;
+
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+
+public class CircleGenerate extends JFrame{
+    ArrayList<Circle> circleList = new ArrayList<>();
+
+
+    public CircleGenerate(int n) {
+        for(int i = 0; i < n; i++) {
+            Circle c = new Circle();
+            this.add(c);
+        }
+        this.setVisible(true);
+    }
+
+    public void targetColor() {
+
+    }
+
+}
