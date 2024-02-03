71e0d599ef568a3630cf939420f59a029d1776e0
peakp <peakpeakpeakhaha@gmail.com>
2024-02-03 12:33:25 +0700

    Update generate

--- CircleGen/CircleGenerate.java
+++ CircleGen/CircleGenerate.java
@@ -9,12 +9,19 @@ import color_lab.Circle;
 
 public class CircleGenerate extends JFrame{
     private ArrayList<JButton> CircleList = new ArrayList<>();
+    
+    static int level;
+    static float randomrange;
+    static float randomHue;
+    static float randomSaturation;
+    static float randomValue;
 
 
-    public CircleGenerate(float hue, float saturation, float value, int n) {
+
+    public CircleGenerate(float randomHue,float randomSaturation,float randomValue,int n) {
         for(int i = 0; i < n; i++) {
             JButton b = new JButton();
-            Circle c = new Circle(hue, saturation, value);
+            Circle c = new Circle(randomHue, randomSaturation, randomValue);
             c.setTarget((Math.random()>0.5));
             c.setRGB();
             b.setBackground(c.getC());
@@ -24,4 +31,20 @@ public class CircleGenerate extends JFrame{
 
         this.setVisible(true);
     }
+    
+    static int getNCircle(int level){
+        if(level>0 && level <=5){
+            return 2;
+        }else if(level>5 && level<=10){
+            return 4;
+        }else if(level>10 && level<=15){
+            return 9;
+        }else{
+            return 16;
+        }
+    }
+    
+    static float makeRandomRange(int level){
+        return 0;  //will implements later
+    }   
 }
