a0e4abf8335cf9e317024059b7bb9d26bb13549e
Akeanant Poomdeesittinon <aekanan2004@gmail.com>
2024-02-09 14:30:16 +0700

    addSoundWhenEndTime

--- MainFrame/lobbyEvent.java
+++ MainFrame/lobbyEvent.java
@@ -71,6 +71,7 @@ public class lobbyEvent  implements ActionListener {
                 if(counter < 0){
                     System.out.println("Stop");
                     timer.cancel();
+                    StageController.playSound("img/incorrect.wav");
                     StageOne.stageOne.setEnabled(false);
                     StageController.lose();
                 }
